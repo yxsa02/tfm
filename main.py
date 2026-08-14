6b901e21446bccc6e4981d2ae2fd5771f9832ce2
yxs <atwyxsa02@163.com>
2026-08-15 03:45:14 +0800

    C:创建util.py用于存储部分函数

--- main.py
+++ main.py
@@ -1,48 +1,7 @@
-import sys,os,shutil,time,unicodedata
+import sys,os,shutil,time
 import v,key
-
-def get_display_width(text: str) -> int:
-    """
-    使用 unicodedata 计算显示宽度
-    """
-    width = 0
-    for char in text:
-        # East Asian Width 属性
-        east_asian_width = unicodedata.east_asian_width(char)
-        if east_asian_width in ('F', 'W'):  # Fullwidth, Wide
-            width += 2
-        elif east_asian_width in ('H', 'Na'):  # Halfwidth, Narrow
-            width += 1
-        elif east_asian_width == 'A':  # Ambiguous (在中文环境下通常算2)
-            width += 2
-        else:  # Neutral
-            width += 1
-    return width
-def str2long(string: str, long: int, b="", s=" ") -> str:
-    """
-    使用 unicodedata 计算显示宽度
-    """
-    if long <= 0:
-        return ""
-    current_width = get_display_width(string)
-    if current_width == long:
-        return string
-    elif current_width < long:
-        padding_needed = long - current_width
-        return string + s * padding_needed
-    else:
-        # 截断
-        result = ""
-        width = 0
-        for char in string:
-            char_width = 2 if unicodedata.east_asian_width(char) in ('F', 'W', 'A') else 1
-            if width + char_width <= long - 1:
-                result += char
-                width += char_width
-            else:
-                break
-        return result + b
-    
+from util import *
+   
 class tfmApp:
     """终端文件管理器"""
     def __init__(self,path:str) -> None:
